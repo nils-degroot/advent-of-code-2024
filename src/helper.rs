0d3e6c9207c1b9de165a6765cdb43f38f2143ae8
nils-degroot <nils@peeko.nl>
2024-11-25 09:30:44 +0100

    Improve hashmap macro

--- src/helper.rs
+++ src/helper.rs
@@ -2,14 +2,31 @@
 macro_rules! hashmap {
     () => {{
         use std::collections::HashMap;
+
         HashMap::new();
     }};
     ( $( $key:expr => $value:expr ),+ $(,)? ) => {{
         use std::collections::HashMap;
-        let mut map = HashMap::new();
+        use $crate::count_tts;
+
+        let mut map = HashMap::with_capacity(count_tts!($($key)+));
+
         $(
             map.insert($key, $value);
         )+
+
         map
     }};
+}
+
+// From https://veykril.github.io/tlborm/decl-macros/building-blocks/counting.html#bit-twiddling
+#[macro_export]
+macro_rules! count_tts {
+    () => { 0 };
+    ($odd:tt $( $a:tt $b:tt )*) => {
+        ( count_tts!($( $a )* ) << 1) | 1
+    };
+    ($( $a:tt $even:tt )*) => {
+        count_tts!( $( $a )* ) << 1
+    };
 }
