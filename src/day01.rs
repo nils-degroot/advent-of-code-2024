69900cce46eff67f254c67db9e4ef4accda328a4
nils-degroot <nils@peeko.nl>
2024-12-01 09:25:11 +0100

    Day 01

--- src/day01.rs
+++ src/day01.rs
@@ -1,67 +1,75 @@
-use crate::hashmap;
+use std::collections::HashMap;
 
 pub fn part1(input: String) -> impl ToString {
-    input
+    let (mut lhs, mut rhs) = input
         .lines()
         .map(|line| {
-            let p = line
-                .chars()
-                .filter(|c| c.is_ascii_digit())
-                .collect::<Vec<_>>();
-
-            let head = p.first().expect("Could not find head");
-            let tail = p.last().expect("Could not find tail");
-
-            format!("{head}{tail}")
-                .parse::<usize>()
-                .expect("Could not parse piece")
+            let mut split = line.split_whitespace();
+            (
+                split
+                    .next()
+                    .expect("Could not find lhs")
+                    .parse::<usize>()
+                    .expect("Could not parse number"),
+                split
+                    .next()
+                    .expect("Could not find rhs")
+                    .parse::<usize>()
+                    .expect("Could not parse number"),
+            )
         })
+        .fold(
+            (vec![], vec![]),
+            |(mut lhs_acc, mut rhs_acc), (lhs, rhs)| {
+                lhs_acc.push(lhs);
+                rhs_acc.push(rhs);
+                (lhs_acc, rhs_acc)
+            },
+        );
+
+    lhs.sort();
+    rhs.sort();
+
+    lhs.into_iter()
+        .zip(rhs)
+        .map(|(lhs, rhs)| lhs.abs_diff(rhs))
         .sum::<usize>()
 }
 
 pub fn part2(input: String) -> impl ToString {
-    let lookup = hashmap! {
-        "one" => 1,
-        "two" => 2,
-        "three" => 3,
-        "four" => 4,
-        "five" => 5,
-        "six" => 6,
-        "seven" => 7,
-        "eight" => 8,
-        "nine" => 9,
-    };
-
-    input
+    let (lhs, rhs) = input
         .lines()
         .map(|line| {
-            let (lhs, _) = lookup
-                .iter()
-                .flat_map(|(text, digit)| {
-                    [
-                        line.find(&digit.to_string()).map(|pos| (digit, pos)),
-                        line.find(text).map(|pos| (digit, pos)),
-                    ]
-                })
-                .flatten()
-                .min_by_key(|(_, p)| *p)
-                .expect("Could not find head");
-
-            let (rhs, _) = lookup
-                .iter()
-                .flat_map(|(text, digit)| {
-                    [
-                        line.rfind(&digit.to_string()).map(|pos| (digit, pos)),
-                        line.rfind(text).map(|pos| (digit, pos)),
-                    ]
-                })
-                .flatten()
-                .max_by_key(|(_, p)| *p)
-                .expect("Could not find tail");
-
-            format!("{lhs}{rhs}")
-                .parse::<usize>()
-                .expect("Failed to parse values")
+            let mut split = line.split_whitespace();
+            (
+                split
+                    .next()
+                    .expect("Could not find lhs")
+                    .parse::<usize>()
+                    .expect("Could not parse number"),
+                split
+                    .next()
+                    .expect("Could not find rhs")
+                    .parse::<usize>()
+                    .expect("Could not parse number"),
+            )
         })
+        .fold(
+            (vec![], HashMap::<usize, usize>::new()),
+            |(mut lhs_acc, mut rhs_acc), (lhs, rhs)| {
+                lhs_acc.push(lhs);
+
+                if let Some(v) = rhs_acc.get_mut(&rhs) {
+                    *v += 1;
+                } else {
+                    rhs_acc.insert(rhs, 1);
+                }
+
+                (lhs_acc, rhs_acc)
+            },
+        );
+
+    lhs.into_iter()
+        .map(|n| rhs.get(&n).unwrap_or(&0) * n)
         .sum::<usize>()
 }
